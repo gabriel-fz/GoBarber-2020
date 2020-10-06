1119483ff517e7855a18d0527c4185961b00b05a
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-06 18:57:01 -0300

    backend: Rotas autenticadas concluido

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'a95c530a7af5f492a74499e70578d150', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '99d',
+      expiresIn,
     });
 
     return {
